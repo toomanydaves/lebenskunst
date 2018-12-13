846e89fed3cdd577f008787217e08466ce1e6318
Sean Jacke <sean.jacke@learnship.com>
2018-12-13 13:28:46 +0100

    directory and catalog apps. Custom user model at directory.User. Initial migrations

---
+++ django_proj/catalog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+'''
+Product
+- .Design
+
+Design
+  - Designer (fk directory.Designer)
+
+Item
+- Product
+'''
--- django_proj/lebenskunst/settings.py
+++ django_proj/lebenskunst/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e^@6+pc7(ce-)=2ykdwt+@nsyc1ma@l5fj86k4^7*z(x5^^msf'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'catalog.apps.CatalogConfig',
+    'directory.apps.DirectoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,14 +72,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lebenskunst.wsgi.application'
 
+# Authentication
+# https://docs.djangoproject.com/en/2.1/topics/auth/customizing
+AUTH_USER_MODEL = 'directory.User'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
     }
 }
 
@@ -118,3 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "lebenskunst/static/lebenskunst"),
+]
+
+# File uploads
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
