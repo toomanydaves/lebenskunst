31ca1f732e3056b461964890214bd24fe28e7159
Sean Jacke <sean.jacke@learnship.com>
2018-12-14 11:13:44 +0100

    adds notes for models

--- django_proj/catalog/models.py
+++ django_proj/catalog/models.py
@@ -1,12 +1,41 @@
 from django.db import models
 
 '''
-Product
-- .Design
-
 Design
-  - Designer (fk directory.Designer)
+- id (auto-generated)
+- name
+- description
+
+Design_Version
+- id (auto-generated)
+- design_id (fk Design.id)
+- description
+- date_created
+- material_costs
+- cost_breakdown
+
+Design_Version_Designer
+- id (auto-generated)
+- designer_id (fk directory.Designer)
+- design_version_id (fk Design_Version)
+
+Product
+- id (auto-generated)
+- design_id (unique, fk Design.id)
+- name
+- description
 
 Item
-- Product
+- id (auto-generated)
+- product_id (fk Product)
+- design_version_id (fk Design_Version.id)
+- date_begun
+- date_completed
+- selling_price
+
+Item_Maker
+- item_id (fk Item)
+- maker_id (fk directory.Maker)
+- production_time
+- commision_requested
 '''
--- django_proj/directory/models.py
+++ django_proj/directory/models.py
@@ -1,4 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 
+'''
+User
+- extends AbstractUser
+
+Designer
+- id (auto-generated)
+- user_id (fk User.id)
+- bio
+
+Maker
+- id (auto-generated)
+- user_id (fk User.id)
+- bio
+
+'''
+
 class User(AbstractUser):
     pass
